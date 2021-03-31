b158b73ab5546fede3b2b1acab15fdf7441fff02
Raul <raulmedeiros9@gmail.com>
2021-03-30 22:49:36 -0300

    Erros and more mistakes

---
+++ Coração de Ferro/GoToTesteAgilidade.gd
@@ -0,0 +1,6 @@
+extends Button
+
+
+func _on_GoToTesteAgilidade_pressed():
+	get_tree().change_scene("res://TesteAgilidade.tscn")
+	
---
+++ Coração de Ferro/Spawner.gd
@@ -0,0 +1,25 @@
+extends Position2D
+
+export (PackedScene) var spawnScene
+onready var spawnerReference = load (spawnScene.get_path())
+ 
+export (NodePath) var timerPath
+onready var timerNode = get_node(timerPath)
+
+export (float) var minTime
+export (float) var maxTime
+
+func _ready():
+	randomize()
+	timerNode.set_wait_time(rand_range(minTime, maxTime))
+	timerNode.start()
+	
+	
+func _on_Timer_timeout():
+	var spawnInstance = spawnerReference.instance()
+	
+	get_parent().add_child(spawnInstance)
+	spawnInstance.set_pos(50, 50)
+	
+	timerNode.set_wait_time(rand_range(minTime, maxTime))
+	timerNode.start()
