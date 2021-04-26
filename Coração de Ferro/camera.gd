68f12eee5bba8fa5dfa3f65ad059deccdb531dd2
Rodrigo Monteiro <rodrigo_m_rodrigues@hotmail.com>
2021-04-26 16:54:08 -0300

    Cena Camera

---
+++ Coração de Ferro/camera.gd
@@ -0,0 +1,38 @@
+extends Sprite
+
+export (NodePath) var luz_camera
+export (NodePath) var Area2d
+export (NodePath) var timerPath
+
+onready var luz_camera_2 = get_node(luz_camera)
+onready var area2d_2 = get_node(Area2d)
+onready var timerNode = get_node(timerPath)
+
+var ligada = true
+
+func _ready():
+	timerNode.set_wait_time(2)
+	timerNode.start()
+	
+func _on_Timer_timeout():
+	
+	if ligada == true:
+		luz_camera_2.set_enabled(false)
+		area2d_2.set_monitoring(false)
+		ligada = false
+		print("Desliguei")
+	else:
+		luz_camera_2.set_enabled(true)
+		area2d_2.set_monitoring(true)
+		ligada = true
+	
+	
+	timerNode.set_wait_time(2)
+	timerNode.start()
+
+
+func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
+	if body.name == "Player":
+		get_tree().change_scene("res://Fase_1.tscn")
+	
+
