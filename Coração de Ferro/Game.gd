2e3c2b125dc331c5b02822df5ec7eb5263158003
Raul <raulmedeiros9@gmail.com>
2021-04-26 19:35:56 -0300

    tile em node pra todo mundo usar

--- Coração de Ferro/Game.gd
+++ Coração de Ferro/Game.gd
@@ -6,32 +6,32 @@ var level = 0
 var movments = [5,30,50]
 var spots
 
-func _process(_delta):
+func _process(_delta): #escrita dos movimentos e contagem dos spots
 	#$Level_Counter.text = 'Level 1'
 	spots = $Spots.get_child_count()
 	$Ui/Remaining_Moves.text = 'MOVIMENTOS RESTANTES: ' + str(moves)
 	
 	
-func check_end():
+func check_end(): #verifica quando o player termina de se movimentar
 	if game_end == false:
-		for i in $Spots.get_children():
+		for i in $Spots.get_children(): 
 			if i.occupied:
 				spots -= 1
 		if moves < 0:
-			$Ui/LoseDialog.popup_centered()
+			$Ui/LoseDialog.popup_centered() #ativa a box de derrota
 			game_end = true
 		elif spots < 1:
-			$Ui/WinDialog.popup_centered()
+			$Ui/WinDialog.popup_centered() #ativa a box de vitoria
 			game_end = true
 		elif moves == 0:
-			$Ui/LoseDialog.popup_centered()
+			$Ui/LoseDialog.popup_centered() #ativa a box de derrota
 			game_end = true
 
 
 
-func _on_WinDialog_confirmed():
+func _on_WinDialog_confirmed():  #acontece quanto da ok na caixa de vitoria
 	get_tree().reload_current_scene()
 
 
-func _on_LoseDialog_confirmed():
+func _on_LoseDialog_confirmed(): #acontece quanto da ok na caixa de derrota
 	get_tree().reload_current_scene()
