0566489b5f828e73b32b60d48fe9de626c0369d4
Rodrigo Monteiro <rodrigo_m_rodrigues@hotmail.com>
2021-04-26 18:23:19 -0300

    Serra giratoria

--- Coração de Ferro/Serra.gd
+++ Coração de Ferro/Serra.gd
@@ -1,13 +1,11 @@
 extends RigidBody2D
 
-export (NodePath) var sprite
-onready var sprite2 = get_node(sprite)
-
 func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
-	print("bati punheta")
 	
 	if body.name == "Player":
-		get_tree().change_scene("res://Fase_1.tscn")
+		get_tree().reload_current_scene()
 	
-func _physics_process(delta):
-	sprite2.set_rotation_degrees(50)
+
+func _on_AnimationRotao_ready():
+	get_node(get_child(3).get_path()).play("Rotation")
+	
