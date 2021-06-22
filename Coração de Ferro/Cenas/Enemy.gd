f9f1c19b278373f4489eee4aa1559e9e071a0343
Bolo (Erick Hasse) <crow16bits@gmail.com>
2021-06-22 12:37:10 -0300

    Inimigo pronto

--- Coração de Ferro/Cenas/Enemy.gd
+++ Coração de Ferro/Cenas/Enemy.gd
@@ -2,21 +2,41 @@ extends KinematicBody2D
 
 
 const GRAVITY = 10
-const SPEED = 30
+export var speed = 30
 const FLOOR = Vector2(0, -1)
+
+export (bool)var quina = true
+export (bool)var parede = true
 
 var velocity = Vector2()
 
-# Called when the node enters the scene tree for the first time.
+var direction = 1
+
+
 func _ready():
 	pass 
 
 
 func _physics_process(delta):
-	velocity.x = SPEED
+	velocity.x = speed * direction
 	
 	$AnimatedSprite.play("walk")
+	
+	if(direction == 1):
+		$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.flip_h = true
+	
 	
 	velocity.y += GRAVITY
 	
 	velocity = move_and_slide(velocity, FLOOR)
+	
+	
+	if is_on_wall() && parede:
+		direction = direction * -1
+		$RayCast2D.position.x *= -1
+	
+	if $RayCast2D.is_colliding() == false && quina:
+		direction = direction * -1
+		$RayCast2D.position.x *= -1
