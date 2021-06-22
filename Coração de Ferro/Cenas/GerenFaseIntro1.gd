07a692128b0bd49a52848aabc4f1a8f38636f50b
LionMRK <rodrigo_m_rodrigues@hotmail.com>
2021-06-22 11:54:04 -0300

    Fase introdução 1 completa

--- Coração de Ferro/Cenas/GerenFaseIntro1.gd
+++ Coração de Ferro/Cenas/GerenFaseIntro1.gd
@@ -1,2 +1,23 @@
-extends Node
+extends Node2D
 
+
+
+func _on_TutorialPulo_body_shape_entered(body_id, body, body_shape, area_shape):
+	if(body.name == "Player"):
+		get_child(0).set_visible_characters(23)
+
+
+func _on_TutorialPulo_body_shape_exited(body_id, body, body_shape, area_shape):
+	if(body.name == "Player"):
+		get_child(0).set_visible_characters(0)
+		
+
+
+func _on_TutorialAndar_body_shape_entered(body_id, body, body_shape, area_shape):
+	if(body.name == "Player"):
+		get_child(1).set_visible_characters(29)
+
+
+func _on_TutorialAndar_body_shape_exited(body_id, body, body_shape, area_shape):
+	if(body.name == "Player"):
+		get_child(1).set_visible_characters(0)
