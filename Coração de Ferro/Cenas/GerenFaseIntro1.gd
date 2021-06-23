8bc43d8df1e5e534a441b14be039cc5dbb84a143
LionMRK <rodrigo_m_rodrigues@hotmail.com>
2021-06-22 21:25:50 -0300

    comeco fase intro 3

--- Coração de Ferro/Cenas/GerenFaseIntro1.gd
+++ Coração de Ferro/Cenas/GerenFaseIntro1.gd
@@ -21,3 +21,8 @@ func _on_TutorialAndar_body_shape_entered(body_id, body, body_shape, area_shape)
 func _on_TutorialAndar_body_shape_exited(body_id, body, body_shape, area_shape):
 	if(body.name == "Player"):
 		get_child(1).set_visible_characters(0)
+
+
+func _on_movimentoSerra_ready():
+	$movimentoSerra.play("movimento")
+	$movimentoSerra2.play("movimento")
