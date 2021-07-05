b0534757976138d01cff32ee0969314b8f38d000
LionMRK <rodrigo_m_rodrigues@hotmail.com>
2021-07-05 17:16:23 -0300

    cutscene 1 terminada

--- Coração de Ferro/Cenas/gerenciadorCustscene.gd
+++ Coração de Ferro/Cenas/gerenciadorCustscene.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+export (String) var fase
+
+onready var fase2 = fase
+
 func _ready():
 	$AnimationPlayer.play("cutscene")
 
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if(anim_name == "cutscene"):
+		get_tree().change_scene("res://Cenas/" + fase2 + ".tscn")
